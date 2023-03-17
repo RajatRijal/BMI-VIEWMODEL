0dcb08669efdffa416eb7db71ea5c17e31bdf1a2
Rajat <rajatrijal26@gmail.com>
2023-03-17 21:29:02 +0200

    viewmodel

--- app/src/main/java/com/example/bmi/MainActivity.kt
+++ app/src/main/java/com/example/bmi/MainActivity.kt
@@ -1,14 +1,9 @@
 package com.example.bmi
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -23,6 +18,20 @@ import kotlin.properties.ReadWriteProperty
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.stringResource
+import com.example.bmi.MyViewModel
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.bmi.ui.theme.BMITheme
 
 
 class MainActivity : ComponentActivity() {
@@ -43,43 +52,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Bmi() {
-    var heightInput: String by remember { mutableStateOf("") }
-    var weightInput: String by remember{ mutableStateOf("") }
-    val height = heightInput.toFloatOrNull() ?: 0.0f
-    val weight = weightInput.toIntOrNull() ?: 0
-    val bmi = if (weight > 0 && height >0)weight / (height * height) else 0.0
+fun Bmi (myViewModel: MyViewModel =  MyViewModel()) {
 
-   Column(
-       modifier =  Modifier.padding(8.dp),
-       verticalArrangement = Arrangement.spacedBy(8.dp)
-   ) {
-       Text(text = "Body mass index" ,
-           fontSize = 24.sp,
-           color = MaterialTheme.colors.primary,
-           textAlign = TextAlign.Center,
-           modifier = Modifier
-               .fillMaxWidth()
-               .padding(top = 16.dp, bottom = 16.dp)
-       )
-       OutlinedTextField(value = heightInput,
-           onValueChange = {heightInput = it.replace( ',','.')},
-           label = { Text(text = "Height")},
-           singleLine = true,
-           keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
-       )
-       OutlinedTextField(
-           value = weightInput,
-           onValueChange ={weightInput= it.replace(',','.')},
-           label = { Text(text = "weight")},
-           singleLine = true,
-           keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-           modifier = Modifier.fillMaxWidth()
-           )
-       Text(text = stringResource(R.string.body_mass_index) + bmi)
-   }
+    Column {
+        Text(
+            text = stringResource(R.string.body_mass_index),
+            fontSize = 24.sp,
+            color = MaterialTheme.colors.primary,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp, bottom = 16.dp)
+        )
+
+        OutlinedTextField(
+            value=myViewModel.heightInput,
+            onValueChange = {myViewModel.changeHeightInput(it)},
+            label = { Text( stringResource(R.string.height))},
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+
+        OutlinedTextField(
+            value= myViewModel.weightInput,
+            onValueChange = {myViewModel.changeWeightInput(it)},
+            label = { Text(stringResource(R.string.weight))},
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+
+        Text(text = stringResource(R.string.result,myViewModel.bmi))
+
+
+    }
+
 }
-
 
 
 
